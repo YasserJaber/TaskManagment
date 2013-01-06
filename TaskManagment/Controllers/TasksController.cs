b0d8094db5caa72641f574868162fde1e74247e4
Yasser Jaber <yjaber@integrant.com>
2013-01-06 18:00:39 +0300

    First push for TaskManagment system

---
+++ TaskManagment/Controllers/TasksController.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TaskManagment.Models;
+
+namespace TaskManagment.Controllers
+{
+    public class TasksController : Controller
+    {
+        private TaskManagmentContext db = new TaskManagmentContext();
+
+        //
+        // GET: /Tasks/
+
+        public ActionResult Index()
+        {
+            return View(db.Tasks.ToList());
+        }
+
+        //
+        // GET: /Tasks/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        //
+        // GET: /Tasks/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Tasks/Create
+
+        [HttpPost]
+        public ActionResult Create(Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tasks.Add(task);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(task);
+        }
+
+        //
+        // GET: /Tasks/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        //
+        // POST: /Tasks/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(task).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(task);
+        }
+
+        //
+        // GET: /Tasks/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Task task = db.Tasks.Find(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        //
+        // POST: /Tasks/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Task task = db.Tasks.Find(id);
+            db.Tasks.Remove(task);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TaskManagment/Models/Task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TaskManagment.Models
+{
+    public class Task
+    {
+        [Required]
+        [Display(Name = "Task ID")]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Task Name")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Due Date")]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", NullDisplayText = "", ConvertEmptyStringToNull = true, ApplyFormatInEditMode = true, HtmlEncode = true)]
+        public DateTime? DueDate { get; set; }
+    }
+}
